6bf476146c0b46a4506e6a15c23dc08fdb9d89b2
Khaledaljasmi <talfrhan@gmail.com>
2022-08-03 19:32:22 +0300

    cw1 done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var name = "Khaled"
+
+var favouriteNumber = 10.56
+
+var hobby = "football"
+
+let name2 = "Adnan"
+
+let birthday = 1997
+
+print("Hello my name is \(name), my favourite number is \(favouriteNumber), and my hobby is \(hobby)")
+ 
+
+
